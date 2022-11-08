d23f4d263d8556143d9df6afa3171cfc1172eca5
viveo-levi <106278007+viveo-levi@users.noreply.github.com>
2022-11-08 17:19:17 -0300

    Update installAgents.bat

--- installAgents.bat
+++ installAgents.bat
@@ -4,7 +4,7 @@ mkdir "%PROGRAMFILES%\Zabbix" && cd "%PROGRAMFILES%\Zabbix"
 curl https://cdn.zabbix.com/zabbix/binaries/stable/6.2/6.2.1/zabbix_agent2-6.2.1-windows-amd64-openssl-static.zip -o zabbix.zip
 powershell Expand-Archive "%PROGRAMFILES%\Zabbix\zabbix.zip" -DestinationPath "%PROGRAMFILES%\Zabbix\" && del "%PROGRAMFILES%\Zabbix\zabbix.zip"
 curl https://raw.githubusercontent.com/viveo-levi/viveoObs/main/zabbix_agent2.conf -o "%PROGRAMFILES%\Zabbix\conf\zabbix_agent2.conf"
-"%PROGRAMFILES%\Zabbix\bin\zabbix_agent2.exe" --config %PROGRAMFILES%\Zabbix\conf\zabbix_agent2.conf --install
+"%PROGRAMFILES%\Zabbix\bin\zabbix_agent2.exe" --config "%PROGRAMFILES%\Zabbix\conf\zabbix_agent2.conf" --install
 sc query "Zabbix Agent 2" | find "RUNNING" || net start "Zabbix Agent 2"
 echo "Finalizando instalacao agente Zabbix"
 echo "Iniciando instalacao agente Elastic"
